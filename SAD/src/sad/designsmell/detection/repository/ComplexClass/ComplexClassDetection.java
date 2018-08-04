265918264e2bd3e493578bbad4519795d745d6a3
Vassilis Zafeiris <bzafiris@gmail.com>
2018-08-04 18:40:40 +0300

    code formatting

--- SAD/src/sad/designsmell/detection/repository/ComplexClass/ComplexClassDetection.java
+++ SAD/src/sad/designsmell/detection/repository/ComplexClass/ComplexClassDetection.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2001-2014 Yann-Gaël Guéhéneuc and others.
+ * Copyright (c) 2001-2014 Yann-Gaï¿½l Guï¿½hï¿½neuc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * 
  * Contributors:
- *     Yann-Gaël Guéhéneuc and others, see in file; API and its implementation
+ *     Yann-Gaï¿½l Guï¿½hï¿½neuc and others, see in file; API and its implementation
  ******************************************************************************/
 package sad.designsmell.detection.repository.ComplexClass;
 
@@ -42,7 +42,7 @@ public class ComplexClassDetection extends AbstractDesignSmellDetection implemen
 		this.operators = OperatorsCodeSmells.getInstance();
 		this.relations = Relationships.getInstance();
 	}
-	
+
 	public String getName() {
 		return "ComplexClass";
 	}
@@ -51,30 +51,28 @@ public class ComplexClassDetection extends AbstractDesignSmellDetection implemen
 		return "../SAD Rules Creator/rsc/ComplexClass.rules";
 	}
 
-	
-public void detect(final IAbstractLevelModel anAbstractLevelModel) {
-final Set candidateDesignSmells = new HashSet();
+	public void detect(final IAbstractLevelModel anAbstractLevelModel) {
+		final Set candidateDesignSmells = new HashSet();
 
-final ICodeSmellDetection csComplexClassOnly = new ComplexClassOnlyDetection();
-csComplexClassOnly.detect(anAbstractLevelModel);
-final Set setComplexClassOnly = ((ComplexClassOnlyDetection) csComplexClassOnly).getCodeSmells();
+		final ICodeSmellDetection csComplexClassOnly = new ComplexClassOnlyDetection();
+		csComplexClassOnly.detect(anAbstractLevelModel);
+		final Set setComplexClassOnly = ((ComplexClassOnlyDetection) csComplexClassOnly).getCodeSmells();
 
-final ICodeSmellDetection csLargeClassOnly = new LargeClassOnlyDetection();
-csLargeClassOnly.detect(anAbstractLevelModel);
-final Set setLargeClassOnly = ((LargeClassOnlyDetection) csLargeClassOnly).getCodeSmells();
+		final ICodeSmellDetection csLargeClassOnly = new LargeClassOnlyDetection();
+		csLargeClassOnly.detect(anAbstractLevelModel);
+		final Set setLargeClassOnly = ((LargeClassOnlyDetection) csLargeClassOnly).getCodeSmells();
 
-final Set setComplexClass = 
-this.operators.union(setLargeClassOnly,setComplexClassOnly);
+		final Set setComplexClass = this.operators.union(setLargeClassOnly, setComplexClassOnly);
 
-final Iterator iterSet = setComplexClass.iterator();
-while(iterSet.hasNext()) {
-final ICodeSmell aCodeSmell = (ICodeSmell) iterSet.next();
-final DesignSmell designSmell = new DesignSmell(aCodeSmell);
-designSmell.setName("ComplexClass");
-final String definition = "To defined";
-designSmell.setDefinition(definition);
-candidateDesignSmells.add(designSmell);
-}
-this.setSetOfDesignSmells(candidateDesignSmells);
-}
+		final Iterator iterSet = setComplexClass.iterator();
+		while (iterSet.hasNext()) {
+			final ICodeSmell aCodeSmell = (ICodeSmell) iterSet.next();
+			final DesignSmell designSmell = new DesignSmell(aCodeSmell);
+			designSmell.setName("ComplexClass");
+			final String definition = "To defined";
+			designSmell.setDefinition(definition);
+			candidateDesignSmells.add(designSmell);
+		}
+		this.setSetOfDesignSmells(candidateDesignSmells);
+	}
 }
